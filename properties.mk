517d235e9c819fb5e544ce3616fef63d135b4778
Deivid21 <david.parra.ignacio@gmail.com>
2021-04-02 04:09:33 -0300

    sdm632-common: Add Moto Camera props
    Signed-off-by: Deivid21 <david.parra.ignacio@gmail.com>

--- properties.mk
+++ properties.mk
@@ -218,3 +218,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.zram.mark_idle_delay_mins=60 \
     ro.zram.first_wb_delay_mins=180 \
     ro.zram.periodic_wb_delay_hours=24
+
+# Expose CFA Config to specific apps
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.camera.cfa.packagelist=com.motorola.coresettingsext,com.motorola.camera2,com.motorola.camera3 \
+    vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.motorola.camera2,com.motorola.camera3,com.motorola.motocit
