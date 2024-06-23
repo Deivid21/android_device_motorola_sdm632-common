b79605b37b5d9969f1a0068b8b7f93d1bc24f373
Michael Bestas <mkbestas@lineageos.org>
2024-06-23 22:51:54 +0300

    sdm632-common: Create rfs fsg symlink
    Change-Id: Id337670d40aeb598e4b2507bd94d4885344c675c

--- common.mk
+++ common.mk
@@ -363,6 +363,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/recovery/root/init.recovery.qcom.rc:root/init.recovery.qcom.rc \
     $(LOCAL_PATH)/recovery/root/init.recovery.qcom.usb.rc:root/init.recovery.qcom.usb.rc
 
+# RFS MSM MPSS symlinks
+PRODUCT_PACKAGES += \
+    rfs_msm_mpss_readonly_vendor_fsg_symlink
+
 # RIL
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.4.vendor \
