b96702d92d1b2672cc0c9f1a2df3d802abcb54ae
Deivid21 <david.parra.ignacio@gmail.com>
2025-03-09 01:58:21 -0300

    sdm632-common: Build MindTheGapps
    Change-Id: I16e4d4df1df4111ff96f1587ed9dbe2a14627015

--- common.mk
+++ common.mk
@@ -148,9 +148,6 @@ PRODUCT_PACKAGES += \
     libfmjni
 
 # GApps
-WITH_GMS := true
-TARGET_USES_PICO_GAPPS := true
-
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
 # GPS
@@ -250,6 +247,9 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml
+
+# MindTheGapps
+ $(call inherit-product, vendor/gapps/arm64/arm64-vendor.mk)
 
 # MotoActions
 PRODUCT_PACKAGES += \
