afe9c1079fde453f55ee088255190303cda2b5a9
Nolen Johnson <johnsonnolen@gmail.com>
2025-04-07 01:20:57 -0400

    fixup! sdm632-common: Switch to common lights HAL
    Change-Id: Ib262092d854c3a9cd5e062e943132e35b78885c3

--- common.mk
+++ common.mk
@@ -195,7 +195,7 @@ PRODUCT_COPY_FILES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.lineage
+    android.hardware.light-service.lineage
 
 # Media
 PRODUCT_COPY_FILES += \
